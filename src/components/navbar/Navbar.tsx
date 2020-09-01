0c230594042c526092e9a965fd1ad8925009bae1
Vjekoslav <vjekoslav.jersic@gmail.com>
2020-09-01 15:42:34 +0200

    Added github icon and fonts, improved Navbar style

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import './Navbar.styles.scss';
+import { ReactComponent as Logo } from '../../assets/icons/github-logo.svg';
 
 const Navbar = () => {
 	return (
 		<nav className="navbar">
 			<div className="container">
-				<div className="github-icon">Ikona</div>
+				<div className="logo-container">
+					<div className="github-text">Github Search</div>
+					<Logo className="github-logo" />
+				</div>
+
 				<div className="links-container">
 					<span className="links">Home</span>
 					<span className="links">Favorites</span>
